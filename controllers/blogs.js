5e8b6de89e56b7e38d37ee477ea4b0fd0ab0b396
Rasmus Harmanen <rasmus@harmanen.fi>
2026-05-19 17:18:59 +0300

    t13

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,78 @@
 const router = require('express').Router()
-const { Blog } = require('../models')
+const jwt = require('jsonwebtoken')
+const { Op } = require('sequelize')
+
+const { SECRET } = require('../util/config')
+const { Blog , User } = require('../models')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+
+  next()
+}
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const where = {}
+
+  if (req.query.search) {
+    where.title = {
+      [Op.iLike]: `%${req.query.search}%`
+    }
+  }
+
+  const blogs = await Blog.findAll({
+    where,
+    include: {
+      model: User,
+      attributes: ['name', 'username']
+    }
+  })
+
   res.json(blogs)
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.create(req.body)
+    const user = await User.findByPk(req.decodedToken.id)
+
+    const blog = await Blog.create({
+      ...req.body,
+      userId: user.id
+    })
+
     res.json(blog)
   } catch (error) {
     next(error)
   }
 })
 
-router.delete('/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
+router.delete('/:id', tokenExtractor, async (req, res, next) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
 
-  if (blog) {
+    if (!blog) {
+      return res.status(404).end()
+    }
+
+    if (blog.userId !== req.decodedToken.id) {
+      return res.status(401).json({ error: 'only the creator can delete the blog' })
+    }
+
     await blog.destroy()
+    res.status(204).end()
+  } catch (error) {
+    next(error)
   }
-
-  res.status(204).end()
 })
 
 router.put('/:id', async (req, res, next) => {
--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 const Blog = require('./blog')
 const User = require('./user')
 
-Blog.sync({ alter: true })
+User.hasMany(Blog)
+Blog.belongsTo(User)
+
 User.sync({ alter: true })
+Blog.sync({ alter: true })
 
 module.exports = {
   Blog,
