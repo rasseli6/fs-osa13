7bd0ee93431532356ed46bdcba8166fe4370abb5
Rasmus Harmanen <rasmus@harmanen.fi>
2026-05-19 16:36:01 +0300

    t6

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -25,4 +25,16 @@ router.delete('/:id', async (req, res) => {
   res.status(204).end()
 })
 
+router.put('/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+
+  if (blog) {
+    blog.likes = req.body.likes
+    await blog.save()
+    res.json(blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
 module.exports = router
