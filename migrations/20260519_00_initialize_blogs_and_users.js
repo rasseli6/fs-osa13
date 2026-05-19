9d99d3913ea8489bc70182372a67f717aca5a3c9
Rasmus Harmanen <rasmus@harmanen.fi>
2026-05-19 18:17:23 +0300

    t18

---
+++ migrations/20260519_00_initialize_blogs_and_users.js
@@ -0,0 +1,76 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    })
+
+    await queryInterface.createTable('blogs', {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      author: {
+        type: DataTypes.TEXT
+      },
+      url: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      likes: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE'
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false
+      }
+    })
+  },
+
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable('blogs')
+    await queryInterface.dropTable('users')
+  }
+}
